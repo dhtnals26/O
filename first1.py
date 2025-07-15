5c447f05fc4e901c7b900eec5f64deff60f82aae
dhtnals26 <24-1519@sonline20.sen.go.kr>
2025-07-15 09:41:02 +0900

    Update first1.py

--- first1.py
+++ first1.py
@@ -1,8 +1,6 @@
-import random # import 함수꾸러미이름
-print(random.choice(['찍먹','부먹'])) # a.b
-
-import random as rd # import 함수꾸러미이름 as 별명
-print(rd.choice(['찍먹','부먹']))
-
-from random import choice # from 함수꾸러미이름 import 함수이름
-print(choice(['찍먹','부먹']))
+import streamlit as st
+st.title('나의 첫 웹 서비스 만들기!!')
+name = st.text_input('이름을 입력해주세요 : ')
+menu = st.selectbox('좋아하는 음식을 선택해주세요:', ['망고빙수','아몬드봉봉'])
+if st.button('인사말 생성') : 
+  st.write(name+'님! 당신이 좋아하는 음식은 '+menu+'이군요?! 저도 좋아요!!')
